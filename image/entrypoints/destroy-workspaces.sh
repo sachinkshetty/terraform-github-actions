7a0714030b708531c697e5c15d38610b00879bab
Sachin Shetty <sachin.shetty@marksandspencer.com>
2021-10-13 00:10:22 +0100

    destroy all workspaces in terraform

--- image/entrypoints/destroy-workspaces.sh
+++ image/entrypoints/destroy-workspaces.sh
@@ -2,17 +2,11 @@
 
 # shellcheck source=../actions.sh
 source /usr/local/actions.sh
-echo "test1"
 debug
-echo "test2"
 setup
-echo "test3"
 init-backend
-echo "test4"
 select-workspace
-echo "test5"
 set-plan-args
-echo "test6"
 # We can't delete an active workspace, so re-initialize with a 'default' workspace (which may not exist)
 workspace=$INPUT_WORKSPACE
 INPUT_WORKSPACE=default
